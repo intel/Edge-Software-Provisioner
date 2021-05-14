2b358d2a5ec59b424add29c6a9669c6b77d10f97
Bryan J Rodriguez <bryan.j.rodriguez@intel.com>
2021-05-14 00:17:27 -0600

    v1.6

--- dockerfiles/gitea/init.sh
+++ dockerfiles/gitea/init.sh
@@ -10,7 +10,8 @@ else
   sleep 5
   gitea manager shutdown
   sed -i 's/INSTALL_LOCK[ \t]*= false/INSTALL_LOCK   = true/' /data/gitea/conf/app.ini && \
-  sed -i 's/\[repository\]/\[repository\]\nDEFAULT_PRIVATE=public\nENABLE_PUSH_CREATE_USER=true\nENABLE_PUSH_CREATE_ORG=true/' /data/gitea/conf/app.ini
+  sed -i 's/\[repository\]/\[repository\]\nDEFAULT_PRIVATE=public\nENABLE_PUSH_CREATE_USER=true\nENABLE_PUSH_CREATE_ORG=true/' /data/gitea/conf/app.ini && \
+  sed -i 's/\[database\]/\[database\]\nLOG_SQL = false/' /data/gitea/conf/app.ini
 fi
 
 if (cat /data/gitea/conf/app.ini | grep INSTALL_LOCK | grep true 2>&1 > /dev/null ); then
